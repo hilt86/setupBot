936196d7a2382df0c42e934c52a7e40d2d89f03e
Hilton De Meillon <hdemeillon@telenet.com.au>
2020-05-29 16:00:32 +1000

    progress

--- setupBot.py
+++ setupBot.py
@@ -6,7 +6,7 @@ class setupBot(BotPlugin):
     This bot does setup stuff
     """
     def get_configuration_template(self):
-        return {'ID_TOKEN': '00112233445566778899aabbccddeeff','URLBASE':'changeme'}
+        return {'ID_TOKEN': 'abc123','URLBASE':'https://www.example.com/'}
 
     @botcmd  
     def tryme(self, msg, args):  # a command callable with !tryme
@@ -15,6 +15,7 @@ class setupBot(BotPlugin):
         Feel free to tweak me to experiment with Errbot.
         You can find me in your init directory in the subdirectory plugins.
         """
-        #token = self.config['ID_TOKEN']
-        #requests.get(self.config['URLBASE'] + token + "/" + args, verify=True)
+        token = self.config['ID_TOKEN']
+        url = self.config['URLBASE']
+        requests.get(str(url) + str(token) + "/" + str(args), verify=True)
         return 'args: ' + str(args) + " message: " + str(msg)
