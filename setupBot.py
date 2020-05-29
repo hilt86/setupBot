4f0879127d7487ed144babae2c8c6acc779db954
Hilton De Meillon <hdemeillon@telenet.com.au>
2020-05-29 15:49:47 +1000

    testing args

--- setupBot.py
+++ setupBot.py
@@ -6,9 +6,9 @@ class setupBot(BotPlugin):
     This bot does setup stuff
     """
     def get_configuration_template(self):
-        return {'ID_TOKEN': '00112233445566778899aabbccddeeff','USERNAME':'changeme'}
+        return {'ID_TOKEN': '00112233445566778899aabbccddeeff','URLBASE':'changeme'}
 
-    @botcmd  # flags a command
+    @botcmd  
     def tryme(self, msg, args):  # a command callable with !tryme
         """
         Execute to check if Errbot responds to command.
@@ -16,5 +16,5 @@ class setupBot(BotPlugin):
         You can find me in your init directory in the subdirectory plugins.
         """
         token = self.config['ID_TOKEN']
-        requests.get("https://ifconfig.co", verify=True)
-        return 'It *works* !'  # This string format is markdown.
+        requests.get(self.config['URLBASE'] + token + "/" + args, verify=True)
+        return 'args: ' + args + " message: " + msg
