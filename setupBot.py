341f6edef4893ae89d5c73d855303568cf15fa3e
Hilton De Meillon <hdemeillon@telenet.com.au>
2020-05-29 15:36:12 +1000

    fixed indentation

--- setupBot.py
+++ setupBot.py
@@ -3,13 +3,10 @@ import requests
 
 class setupBot(BotPlugin):
     """
-    This is a very basic plugin to try out your new installation and get you started.
-    Feel free to tweak me to experiment with Errbot.
-    You can find me in your init directory in the subdirectory plugins.
+    This bot does setup stuff
     """
     def get_configuration_template(self):
-    return {'ID_TOKEN': '00112233445566778899aabbccddeeff',
-            'USERNAME':'changeme'}
+        return {'ID_TOKEN': '00112233445566778899aabbccddeeff','USERNAME':'changeme'}
 
     @botcmd  # flags a command
     def tryme(self, msg, args):  # a command callable with !tryme
