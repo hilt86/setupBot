e876742e080558cbb779f9cce3bc23ac69b7d794
Hilton De Meillon <hdemeillon@telenet.com.au>
2020-05-29 15:31:36 +1000

    testing config

--- setupBot.py
+++ setupBot.py
@@ -1,12 +1,15 @@
 from errbot import BotPlugin, botcmd
+import requests
 
-
-class Example(BotPlugin):
+class setupBot(BotPlugin):
     """
     This is a very basic plugin to try out your new installation and get you started.
     Feel free to tweak me to experiment with Errbot.
     You can find me in your init directory in the subdirectory plugins.
     """
+    def get_configuration_template(self):
+    return {'ID_TOKEN': '00112233445566778899aabbccddeeff',
+            'USERNAME':'changeme'}
 
     @botcmd  # flags a command
     def tryme(self, msg, args):  # a command callable with !tryme
@@ -15,4 +18,6 @@ class Example(BotPlugin):
         Feel free to tweak me to experiment with Errbot.
         You can find me in your init directory in the subdirectory plugins.
         """
+        token = self.config['ID_TOKEN']
+        requests.get("https://ifconfig.co", verify=True)
         return 'It *works* !'  # This string format is markdown.
